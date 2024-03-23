dfd71e1a279c59b9e36f599b6bb223e54178e79b
Iram.Ashfaq <161501174+iram-ashfaq@users.noreply.github.com>
2024-03-23 23:53:42 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Assignments number 07
+var addition = 5 + 3;
+console.log("addition:", addition);
+var subtrsctions = 10 - 2;
+console.log("subtractions:", subtrsctions);
+var multiplication = 4 * 2;
+console.log("multiplication:", multiplication);
+var division = 16 / 2;
+console.log("division:", division);
+//Asignments number 08
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(4 * 2);
+console.log(16 / 2);
+//Asignments number 09
+var favoritenumber = 3;
+var masage = "My favoritenumber: ".concat(favoritenumber);
+console.log(masage);
+//Asignments number 10
+// this program adds two numbers and print the result
+// define two numbers
+var number1 = 10;
+var number2 = 12;
+//add two numbers
+var result = number1 + number2;
+// print result
+console.log(result);
---
+++ main.ts
@@ -0,0 +1,33 @@
+import { Console } from "console";
+
+// Assignments number 07
+let addition: number = 5+3;
+console.log ("addition:", addition);
+let subtrsctions: number =10-2
+console.log ("subtractions:", subtrsctions);
+let multiplication: number =4*2
+console.log ("multiplication:", multiplication);
+let division: number = 16/2;
+console.log ("division:", division);
+
+//Asignments number 08
+console.log(5+3)
+console.log(10-2)
+console.log(4*2)
+console.log(16/2)
+
+//Asignments number 09
+let favoritenumber: number = 3;
+let masage: string = `My favoritenumber: ${ favoritenumber}`;
+console.log (masage);
+
+//Asignments number 10
+// this program adds two numbers and print the result
+// define two numbers
+let number1: number = 10;
+let number2: number = 12;
+//add two numbers
+let result: number = number1 + number2;
+// print result
+console.log(result);
+
